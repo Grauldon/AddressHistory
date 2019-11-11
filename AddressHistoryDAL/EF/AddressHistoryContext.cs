6912e4b8d631463d4d2ff829e871b9d26a5d1b2e
Grauldon <17791090+Grauldon@users.noreply.github.com>
2019-11-11 15:47:42 -0500

    Add EF folder to the DAL project
    Add a context and context factory class to the AddressHistoryDAL
    project. These classes configures Entity Framework for the project.
    
    Signed-off-by: Grauldon <17791090+Grauldon@users.noreply.github.com>
    
      Changes to be committed:
     	new file:   EF/AddressHistoryContext.cs
     	new file:   EF/AddressHistoryContextFactory.cs

---
+++ AddressHistoryDAL/EF/AddressHistoryContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+using AddressHistoryDAL.Models;
+
+namespace AddressHistoryDAL.EF
+{
+    public class AddressHistoryContext : DbContext
+    {
+        internal AddressHistoryContext() {}
+        public AddressHistoryContext(DbContextOptions options) : base(options) {}
+
+        public DbSet<Address> Address { get; set; }
+
+        public string GetTableName(Type type) => Model.FindEntityType(type).SqlServer().TableName;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = @"server=(LocalDb)\MSSQLLocalDB;database=AddressHistory;integrated security=True;
+                    MultipleActiveResultSets=True;App=EntityFramework;";
+                optionsBuilder.UseSqlServer(
+                    connectionString,
+                    options => options.EnableRetryOnFailure())
+                    .ConfigureWarnings(warnings=>warnings.Throw(RelationalEventId.QueryClientEvaluationWarning));
+            }
+        }
+    }
+}
---
+++ AddressHistoryDAL/EF/AddressHistoryContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace AddressHistoryDAL.EF
+{
+    public class AddressHistoryContextFactory : IDesignTimeDbContextFactory<AddressHistoryContext>
+    {
+        public AddressHistoryContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AddressHistoryContext>();
+            var connectionString = @"server=(LocalDb)\MSSQLLocalDB;database=AddressHistory;integrated security=True;
+                    MultipleActiveResultSets=True;App=EntityFramework;";
+            optionsBuilder.UseSqlServer(
+                    connectionString,
+                    options => options.EnableRetryOnFailure())
+                .ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.QueryClientEvaluationWarning));
+            return new AddressHistoryContext(optionsBuilder.Options);
+        }
+    }
+}
