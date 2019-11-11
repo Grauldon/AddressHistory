f98e0674ad7aad4c945d635cdf87d472e7a13104
Grauldon <17791090+Grauldon@users.noreply.github.com>
2019-11-11 15:50:43 -0500

    Add the Address model and interface
    Add the initial versions for the Address Class and the Address
    Interface.
    
    Signed-off-by: Grauldon <17791090+Grauldon@users.noreply.github.com>
    
      Changes to be committed:
     	new file:   ../AddressHistoryDAL.Models/Address.cs
     	new file:   IAddress.cs

---
+++ AddressHistoryDAL.Models/Address.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AddressHistoryDAL.Models
+{
+    public class Address
+    {
+        [DataType(DataType.Date)]
+        public string StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public string EndDate { get; set; }
+
+        [StringLength(50)]
+        public string Address1 { get; set; }
+
+        [StringLength(20)]
+        public string Address2 { get; set; }
+
+        [StringLength(20)]
+        public string City { get; set; }
+
+        [StringLength(2)]
+        public string State { get; set; }
+
+        [StringLength(5)]
+        public string Zip5 { get; set; }
+
+        [StringLength(4)]
+        public string Zip4 { get; set; }
+
+        public List<Address> GetAddresses()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ AddressHistoryDAL/IAddress.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+using AddressHistoryDAL.Models;
+
+namespace AddressHistory.Interfaces
+{
+    interface IAddress
+    {
+        List<Address> GetAddresses();
+    }
+}
