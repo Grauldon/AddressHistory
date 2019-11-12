0d659e620e127bc4503b61410d69852589476807
Grauldon <17791090+Grauldon@users.noreply.github.com>
2019-11-11 21:08:46 -0500

    Rename Interface and create initial version
    Signed-off-by: Grauldon <17791090+Grauldon@users.noreply.github.com>
    
      Changes to be committed:
     	deleted:    IAddress.cs
     	new file:   IRepo.cs

---
+++ AddressHistoryDAL/IRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddressHistoryDAL
+{
+    interface IRepo<T>
+    {
+        T GetAddress(DateTime date);
+        List<T> GetAddresses();
+        List<T> GetAddresses(DateTime date);
+        int Add(T addr);
+        int Update(T addr);
+        int Delete(T addr);
+    }
+}
