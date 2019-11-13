963b8dd70d811cab26c875cf74277bc8cfdcaba6
Grauldon <17791090+Grauldon@users.noreply.github.com>
2019-11-12 22:21:42 -0500

    Initial testing using the console
    Signed-off-by: Grauldon <17791090+Grauldon@users.noreply.github.com>
    
      Changes to be committed:
     	modified:   Program.cs

--- AddressHistory/Program.cs
+++ AddressHistory/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+
+using AddressHistoryDAL;
+using AddressHistoryDAL.Models;
 
 namespace AddressHistory
 {
@@ -6,7 +10,31 @@ namespace AddressHistory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DataRepo<Address> data = new DataRepo<Address>();
+            List<Address> addresses = new List<Address>();
+            Address addr = new Address();
+            DateTime date = new DateTime(2007, 07, 01);
+
+            addresses = data.GetAddresses(date);
+
+            foreach (var item in addresses)
+            {
+                Console.WriteLine($"Between {item.StartDate.ToString("MM/dd/yyyy")} and {item.EndDate.ToString("MM/dd/yyyy")}, you lived at");
+                DisplayAddress(item);
+                Console.WriteLine();
+            }
+
+            //Console.WriteLine();
+
+            //addr = data.GetAddress(date);
+            //Console.WriteLine($"On {date.ToString("MM/dd/yyyy")}, you lived at");
+            //DisplayAddress(addr);
+        }
+
+        static void DisplayAddress(Address address)
+        {
+            Console.WriteLine($"{address.Address1} {address.Address2}");
+            Console.WriteLine($"{address.City}, {address.State} {address.Zip5}-{address.Zip4}");
         }
     }
 }
