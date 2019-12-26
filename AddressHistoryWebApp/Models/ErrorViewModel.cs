b7db7171bb5812112201e8f08c7a8418f61437aa
Grauldon <17791090+Grauldon@users.noreply.github.com>
2019-12-26 10:48:03 -0500

    Add the web application project
    Signed-off-by: Grauldon <17791090+Grauldon@users.noreply.github.com>
    
      Changes to be committed:
     	modified:   AddressHistory.sln
     	new file:   AddressHistoryWebApp/AddressHistoryWebApp.csproj
     	new file:   AddressHistoryWebApp/Controllers/HomeController.cs
     	new file:   AddressHistoryWebApp/Models/ErrorViewModel.cs
     	new file:   AddressHistoryWebApp/Program.cs
     	new file:   AddressHistoryWebApp/Startup.cs
     	new file:   AddressHistoryWebApp/Views/Home/Index.cshtml
     	new file:   AddressHistoryWebApp/Views/Home/Privacy.cshtml
     	new file:   AddressHistoryWebApp/Views/Shared/Error.cshtml
     	new file:   AddressHistoryWebApp/Views/Shared/_CookieConsentPartial.cshtml
     	new file:   AddressHistoryWebApp/Views/Shared/_Layout.cshtml
     	new file:   AddressHistoryWebApp/Views/Shared/_ValidationScriptsPartial.cshtml
     	new file:   AddressHistoryWebApp/Views/_ViewImports.cshtml
     	new file:   AddressHistoryWebApp/Views/_ViewStart.cshtml
     	new file:   AddressHistoryWebApp/appsettings.Development.json
     	new file:   AddressHistoryWebApp/appsettings.json
     	new file:   AddressHistoryWebApp/wwwroot/css/site.css
     	new file:   AddressHistoryWebApp/wwwroot/favicon.ico
     	new file:   AddressHistoryWebApp/wwwroot/js/site.js

---
+++ AddressHistoryWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AddressHistoryWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
