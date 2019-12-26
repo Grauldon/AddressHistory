c7ad7c10af6287cd62239699bd2857c803c08b69
Grauldon <17791090+Grauldon@users.noreply.github.com>
2019-12-26 14:44:37 -0500

    Initial configurations and customizations
    * Add references to the AddressHistoryDAL and AddressHistoryDAL.Models
    projects
    
    * Configured the dependency injection container in Startup.cs
    
    * Configured imports to allow the custom tag helper invocation method
    for view components.
    
    * Customized the _Layout.cshtml file
    
    * Removed Privacy.cshtml and associated code
    
    * Began customization of the home page
    
    Signed-off-by: Grauldon <17791090+Grauldon@users.noreply.github.com>
    
    Changes to be committed:
     	modified:   AddressHistoryWebApp/AddressHistoryWebApp.csproj
     	modified:   AddressHistoryWebApp/Controllers/HomeController.cs
     	modified:   AddressHistoryWebApp/Startup.cs
     	modified:   AddressHistoryWebApp/Views/Home/Index.cshtml
     	deleted:    AddressHistoryWebApp/Views/Home/Privacy.cshtml
     	modified:   AddressHistoryWebApp/Views/Shared/_Layout.cshtml
     	modified:   AddressHistoryWebApp/Views/_ViewImports.cshtml

--- AddressHistoryWebApp/Startup.cs
+++ AddressHistoryWebApp/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+using AddressHistoryDAL.Repos;
+
 namespace AddressHistoryWebApp
 {
     public class Startup
@@ -33,6 +35,7 @@ namespace AddressHistoryWebApp
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddScoped<IDataRepo, DataRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
